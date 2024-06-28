5bd2fa1ffd0626b27091a61ef654a4c623af1d48
drdexter33 <drdexter33@protonmail.com>
2024-06-28 12:45:30 -0400

    Add project files.

---
+++ PizzaStore/Migrations/20240208203030_Initial.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PizzaStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Pizzas",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[] { 1, "Classic Pepperoni Pizza", "Pepperoni" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Pizzas",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
